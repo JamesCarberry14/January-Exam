684513e7041045e56092a138f34053be3d42f77c
James Carberry <S00197582@mail.itsligo.ie>
2021-01-11 11:28:06 +0000

    Added selection changed fuctionality

--- January Exam/Account.cs
+++ January Exam/Account.cs
@@ -9,11 +9,15 @@ namespace January_Exam
 {
     public abstract class Account
     {
+
+        //variables
         public string firstname { get; set; }
         public string lastname { get; set; }
         public decimal balance { get; set; }
         public DateTime interestdate { get; set; }
 
+
+        //constructor
         public Account(string FName, string LName, decimal Balance, DateTime InterestDate)
         {
             firstname = FName;
@@ -22,7 +26,7 @@ namespace January_Exam
             interestdate = InterestDate;
         }
 
-
+        //withdraw method
         public static void Withdraw(decimal balance, decimal value)
         {
             if(balance >= value)
@@ -35,7 +39,7 @@ namespace January_Exam
             }
         }
 
-
+        //deposit method
         public static void Deposit(decimal balance, decimal value)
         {          
                 balance += value;          
@@ -43,7 +47,9 @@ namespace January_Exam
 
 
         public abstract decimal CalculateInterest();
-    
+
+        public override abstract string ToString();
+
     }
 
 
@@ -51,19 +57,20 @@ namespace January_Exam
 
 
 
-
+    //Current Account class
     public class CurrentAccount : Account
     {
+        //variables
         public decimal interestrate { get; set; }
 
 
-
+        //constructor
         public CurrentAccount(string FName, string LName, decimal Balance, DateTime InterestDate) : base(FName, LName, Balance, InterestDate)
         {
             interestrate = 0.03m;
         }
 
-
+        //calculate interest method
         public override decimal CalculateInterest()
         {
             interestdate = DateTime.Now;
@@ -71,19 +78,28 @@ namespace January_Exam
             decimal Interest = balance * interestrate;
             return Interest;
         }
+
+        //tostring method
+        public override string ToString()
+        {
+            return string.Format($"{balance},{lastname}, {firstname} - Current Account");
+        }
+
+
     }
 
 
 
 
 
-
+    //Savings account class
     public class SavingsAccount : Account
     {
+        //variables
         public decimal interestrate { get; set; }
 
 
-
+        //Constructor
         public SavingsAccount(string FName, string LName, decimal Balance, DateTime InterestDate) : base(FName, LName, Balance, InterestDate)
         {
             interestrate = 0.06m;
@@ -91,13 +107,19 @@ namespace January_Exam
 
 
 
-
+        //calculate interest method
         public override decimal CalculateInterest()
         {
             interestdate = DateTime.Now;
 
             decimal Interest = balance * interestrate;
             return Interest;
+        }
+
+        //tostring method
+        public override string ToString()
+        {
+            return string.Format($"{balance},{lastname}, {firstname} - Current Account");
         }
 
     }
--- January Exam/MainWindow.xaml.cs
+++ January Exam/MainWindow.xaml.cs
@@ -31,23 +31,62 @@ namespace January_Exam
             InitializeComponent();
         }
 
-        private void Window_Loaded()
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //adding 4 objects
+            CurrentAccount CA1 = new CurrentAccount("Linda", "Gallagher", 20000, new DateTime(11/1/2020));
+            CurrentAccount CA2 = new CurrentAccount("Katrina", "Walsh", 25000, new DateTime(18/11/2020));
 
-            CurrentAccount CA1 = new CurrentAccount("Linda", "Gallagher", 20000, 11/1/2021);
-            CurrentAccount CA2 = new CurrentAccount("Katrina", "Walsh", 25000, 11/1/2021);
+            SavingsAccount SA1 = new SavingsAccount("Desmond","Jackson", 32000, new DateTime(15/3/2020));
+            SavingsAccount SA2 = new SavingsAccount("John", "Smith", 22000, new DateTime(21/4/2020));
 
-            SavingsAccount SA1 = new SavingsAccount("Desmond","Jackson", 32000, 11/1/2021);
-            SavingsAccount SA2 = new SavingsAccount("John", "Smith", 22000, 11/1/2021);
-
-
+            //adding objects to list
             Accounts.Add(CA1);
             Accounts.Add(CA2);
 
             Accounts.Add(SA1);
             Accounts.Add(SA2);
 
+            //adding list as item source for listbox
             AccountList_lbx.ItemsSource = Accounts;
+
+        }
+
+        private void AccountList_lbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //this method determines which item is selected and prints the details to the textboxes
+
+            Account selectedItem = AccountList_lbx.SelectedItem as Account;
+
+            if (selectedItem != null)
+            {
+                DisplayFName_tblk.Text = selectedItem.firstname;
+                DisplayLName_tblk.Text = selectedItem.lastname;
+                DisplayBalance_tblk.Text = selectedItem.balance.ToString();
+
+
+                if (selectedItem is CurrentAccount)
+                {
+                    DisplayACType_tblk.Text = "Current Account";
+                    DisplayInterest_tblk.Text = "3%";
+                }
+
+                if (selectedItem is SavingsAccount)
+                {
+                    DisplayACType_tblk.Text = "Savings Account";
+                    DisplayInterest_tblk.Text = "6%";
+                }
+
+            }
+        }
+
+        private void Deposit_btn_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Withdraw_btn_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
